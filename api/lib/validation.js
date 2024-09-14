bd7c50af8c2780e9242b2fc18b97c91a31006475
Richard Freitas <wesleyrichard30@gmail.com>
2024-09-14 14:03:27 -0300

    :bug: fix: corrige o regex de validação dos nomes de arquivo

--- api/lib/validation.js
+++ api/lib/validation.js
@@ -1,11 +1,11 @@
 module.exports.validateImageName = (filename) => {
   // Regex to validate filename
-  const regex = /^[a-zA-Z0-9-_]+\.(jpg|jpeg|png)$/;
+  const regex = /^.*\.(jpg|jpeg|png)$/;
   return regex.test(filename);
 };
 
 module.exports.validateAudioName = (filename) => {
   // Regex to validate filename
-  const regex = /^[a-zA-Z0-9-_]+\.(mp3)$/;
+  const regex = /^.*\.(mp3)$/;
   return regex.test(filename);
 };
