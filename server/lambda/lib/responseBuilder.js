1a084c82fab766b69bc10c3867a3aaa5894954a4
Richard Freitas <wesleyrichard30@gmail.com>
2024-09-15 10:25:06 -0300

    :recycle: refactor: ajusta a lógica do response builder do chatbot

--- server/lambda/lib/responseBuilder.js
+++ server/lambda/lib/responseBuilder.js
@@ -1,25 +1,42 @@
-module.exports.handleResponse = (event, type, slotToElicit, messages) => {
-   return {
-      sessionState: {
-         sessionAttributes: event.sessionState.sessionAttributes,
-         intent: event.sessionState.intent,
-         dialogAction: {
-            type: type,
-            slotToElicit: slotToElicit,
-         },
+module.exports.handleResponse = (
+  event,
+  type,
+  slotToElicit,
+  message,
+  imageResponseCard
+) => {
+  const intentState = type === 'Delegate' ? 'ReadyForFulfillment' : 'Fulfilled';
+
+  let messages = [];
+
+  if (Array.isArray(message)) {
+    messages = message.map((message) => ({
+      contentType: 'PlainText',
+      content: message,
+    }));
+  } else if (message) {
+    messages.push({
+      contentType: 'PlainText',
+      content: message,
+    });
+  }
+
+  if (imageResponseCard) {
+    messages.push(imageResponseCard);
+  }
+
+  return {
+    sessionState: {
+      sessionAttributes: event.sessionState.sessionAttributes,
+      intent: {
+        ...event.sessionState.intent,
+        state: intentState,
       },
-      messages: Array.isArray(messages)
-         ? messages.map((message) => ({
-            contentType: 'PlainText',
-            content: message,
-         }))
-         : messages
-            ? [
-               {
-                  contentType: 'PlainText',
-                  content: messages,
-               },
-            ]
-            : [],
-   };
+      dialogAction: {
+        type: type,
+        slotToElicit: slotToElicit,
+      },
+    },
+    messages,
+  };
 };
