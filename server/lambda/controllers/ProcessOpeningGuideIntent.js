1fe3d74bc5c9e2d5adddba6ea85dc6c218a04e08
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-09-18 19:40:43 -0300

    :sparkles: feat: modificando handler para lidar com um dos fluxos da intent ProcessOpeningGuideIntent

--- server/lambda/controllers/ProcessOpeningGuideIntent.js
+++ server/lambda/controllers/ProcessOpeningGuideIntent.js
@@ -1,9 +1,29 @@
+const { getProcessesGuide } = require('../lib/getProcessesGuide');
+const { handleResponse } = require('../lib/responseBuilder');
+
 module.exports.handleProcessOpeningGuideIntent = async (event) => {
-    const response = {
-      
-    
-    };
- 
-    return response;
- };
- 
+  const userInput = event.inputTranscript.toLowerCase();
+
+  // Verifica se o usuário quer ver o passo a passo de como abrir um processo
+  if (['passo', 'a'].some(key => userInput.includes(key))) {
+    try {
+      // Busca o tutorial no DynamoDB
+      const processGuide = await getProcessesGuide();
+
+      // Retorna o tutorial para abrir um processo
+      const msg = 'Aqui está o passo a passo para abrir um processo!';
+      return handleResponse(event, 'Close', null, [msg, processGuide]);
+    } catch (error) {
+      console.error(error);
+
+      // Retorna uma mensagem de erro
+      const msg =
+        'Desculpe, houve um erro ao consultar os processos, tente novamente.';
+      return handleResponse(event, 'Close', null, msg);
+    }
+  }
+
+  // Caso o input não corresponda ao esperado
+  return handleResponse(event, 'Close', null, 'Não entendi seu pedido.');
+};
+
