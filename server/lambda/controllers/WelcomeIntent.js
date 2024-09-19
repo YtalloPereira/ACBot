439f543aff1713dcde71bed1db57c232d34459be
Richard Freitas <wesleyrichard30@gmail.com>
2024-09-19 08:05:39 -0300

    :recycle: refactor: melhora utterances e resposta de contexto da WelcomeIntent

--- server/lambda/controllers/WelcomeIntent.js
+++ server/lambda/controllers/WelcomeIntent.js
@@ -1,42 +1,15 @@
 const { handleResponse } = require("../lib/responseBuilder");
 
 module.exports.handleWelcomeIntent = async (event) => {
+  // Captura o atributo de sessão que indica se a mensagem de boas-vindas já foi enviada, definido no lex
   const welcomeSent = event.sessionState.sessionAttributes?.welcomeSent
 
-  if (welcomeSent === 'true') {
-    const msg = 'Oi, Estou aqui para te ajudar, qual a sua dúvida?';
-    return handleResponse(event, 'Close', null, msg);
+  // Verifica se a mensagem de boas-vindas já foi enviada ao usuário, caso contrário, envia a mensagem
+  if (!welcomeSent) {
+    return handleResponse(event, 'Delegate', null)
   }
-  
-  event.sessionState.sessionAttributes.welcomeSent = 'true'
 
-  const imageResponseCard = {
-    contentType: 'ImageResponseCard',
-    imageResponseCard: {
-      title: 'Veja as opções abaixo para começar!',
-      subtitle: 'Selecione uma das opções para obter mais informações:',
-      buttons: [
-        {
-          text: 'Obtenha orientação sobre como abrir um processo',
-          value: 'Obter orientação sobre como abrir um processo',
-        },
-        {
-          text: 'Descubra sobre os tipos de processos disponíveis',
-          value: 'Descobrir sobre os tipos de processos disponíveis',
-        },
-        {
-          text: 'Inicie um novo processo',
-          value: 'Iniciar um novo processo',
-        },
-        {
-          text: 'Verifique o status dos processos em andamento',
-          value: 'Verificar o status dos processos em andamento',
-        },
-      ],
-    },
-  };
-
-  const msg = 'Olá! Bem-vindo ao assistente de processos acadêmicos.';
-
-  return handleResponse(event, 'Close', null, msg, imageResponseCard);
+  // Define outra mensagem caso o usuário já tenha recebido a mensagem de boas-vindas
+  const msg = 'Oi, Estou aqui para te ajudar, qual a sua dúvida?';
+  return handleResponse(event, 'Close', null, msg);
 } 
